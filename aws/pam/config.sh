4f28cafa81315982f760c917ec87b347943cdb08
Steven Liang <steven.liang@pm.me>
2025-02-22 02:14:29 +0800

    updated pam

--- aws/pam/config.sh
+++ aws/pam/config.sh
@@ -18,9 +18,9 @@ function print_usage(){
     echo "Usage: sudo ./config.sh \$password"
 }
 
-
-private_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+token=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+private_ip=$(curl -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/local-ipv4)
+public_ip=$(curl -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/public-ipv4)
 password=$1
 if [ -z "$private_ip" ] || [ -z "$public_ip" ] || [ -z "$password" ]; then
  print_usage
