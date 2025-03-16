3396add6b3e597fe6ee4b39d0aa7d648b11f4411
Steven Liang <steven.liang@pm.me>
2025-03-17 05:49:03 +0800

    update hcl products

---
+++ hcl/paraview-api-standalone-hcl-trial/RFS/outputs.tf
@@ -0,0 +1,4 @@
+
+output "info" {
+  value = "Welcome to Paraview API Platform. Please wait about 5mins then you should able to access http://${module.eip.ip[0]} with admin and Admin!23&paraview credential."
+}
