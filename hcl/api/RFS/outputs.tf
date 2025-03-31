aa9a2aa4c671d4ec5e4166f4f4753e03bc7283bd
Steven Liang <steven.liang@pm.me>
2025-03-31 21:16:00 +0800

    updated

--- hcl/api/RFS/outputs.tf
+++ hcl/api/RFS/outputs.tf
@@ -1,4 +1,4 @@
 
 output "info" {
-  value = "Welcome to Paraview API Platform. Please wait about 5mins then you should able to access http://${module.eip.ip[0]} with [admin/Admin!123'&'paraview] credential."
+  value = "Welcome to Paraview API Platform. Please wait about 5mins then you should able to access https://${module.eip.ip[0]} with [admin/Admin!123\&paraview] credential."
 }
